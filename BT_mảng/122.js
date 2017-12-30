4f61e31134e0c6f255edb53f18c5cfc8bd4461d6
DESKTOP-J583M93\bienn <bienngocduy@gmail.com>
2017-12-30 09:03:05 +0700

    30.12

---
+++ BT_mảng/122.js
@@ -0,0 +1,12 @@
+//tim gia tri lon nhat trong mang 1 chieu 
+let a= [];
+function bai122(a) {
+    max_value = a[0];
+    for (let i = 0; i <= a.length; i++) {
+        if (a[i] > max_value) {
+            max_value = a[i]
+        }
+    }
+    return max_value;
+}
+console.log(bai122([1,2,3,4, 100]))
