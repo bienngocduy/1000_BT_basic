d35591e609b2915703cdfc467193fac0fcffe3a2
bienngocduy <bienngocduy@gmail.com>
2018-01-05 07:06:50 +0700

    Update bt.js

--- Date - String/bt.js
+++ Date - String/bt.js
@@ -1,6 +1,6 @@
 
 function Week_of_day(weekday1) {
-   weekday1 = ''
+    weekday1 = ''
     full_day = 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'
     last_week_day = 'Saturday, Sunday'
     if (weekday1 == full_day) {
