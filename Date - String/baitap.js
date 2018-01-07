efa7581c288240a2dfa5815ef389e67651b252b5
DESKTOP-J583M93\bienn <bienngocduy@gmail.com>
2018-01-08 00:03:39 +0700

    done

--- Date - String/baitap.js
+++ Date - String/baitap.js
@@ -15,18 +15,41 @@
 //  			Chọn: Monday + Thurday + Sunday
 //  			Kết quả: "Monday, Thursday, Sunday"
 //-------------------------------------------------------
-weekday = ''
+// weekday = ''
+// function day_of_week(weekday) {
+//     full_week = 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'
+//     last_week_day = 'Saturday, Sunday'
+//     if (weekday === full_week) {
+//         console.log('everyday')
+//     }
+//     else if (weekday === last_week_day) {
+//         console.log('weekend')
+//     } else {
+//         result = weekday.replace(/[+,]/g, ',');
+//         console.log(result)
+//     }
+// }
+// day_of_week('')
+
+// func gdayInWeek(str) {
+// 	str = formatdata(str);
+// 	day_of_week(str)
+// }
+
 function day_of_week(weekday) {
-    full_week = 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'
-    last_week_day = 'Saturday, Sunday'
-    if (weekday === full_week) {
-        console.log('everyday')
+    if (weekday == undefined) {
+        return '';
     }
-    else if (weekday === last_week_day) {
-        console.log('weekend')
+    full_day = 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'
+    last_week_day = 'Saturday, Sunday'
+    if (weekday === full_day) {
+        weekday = 'everyday';
+    } else if (weekday === last_week_day) {
+        weekday = 'weekend';
     } else {
         result = weekday.replace(/[+,]/g, ',');
-        console.log(result)
+        weekday = result;
     }
+
+    return weekday;
 }
-day_of_week('')
