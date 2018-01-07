13047602600afd17fedc48dcfbd9e90081c79a4e
DESKTOP-J583M93\bienn <bienngocduy@gmail.com>
2018-01-07 10:18:00 +0700

    123

--- Date - String/baitap.js
+++ Date - String/baitap.js
@@ -1,3 +1,20 @@
+// Trong tuần có 7 ngày:
+// 	Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+// Yêu cầu:
+// 	1>Nếu chọn: Saturday, Sunday 
+//  	Kết quả: "weekend"
+
+//  	2>
+//  	Nếu chọn: Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+//  	Kết quả: "everyday"
+
+//  	3>
+//  	Còn lại: 
+//  	Kết quả: Trả về tên các sự lựa chọn
+//  	Ví dụ: 
+//  			Chọn: Monday + Thurday + Sunday
+//  			Kết quả: "Monday, Thursday, Sunday"
+//-------------------------------------------------------
 weekday = ''
 function Week_of_day(weekday) {
     full_day = 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'
@@ -7,8 +24,7 @@ function Week_of_day(weekday) {
     }
     if (weekday === last_week_day) {
         console.log('weekend')
-    }
-    else {
+    } else {
         result = weekday.replace(/[+,]/g, ',');
         console.log(result)
     }
