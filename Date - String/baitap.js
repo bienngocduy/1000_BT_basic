c6cfe634a574fd093166ca5886cf776da0c0eb20
DESKTOP-J583M93\bienn <bienngocduy@gmail.com>
2018-01-07 23:39:27 +0700

    1234

--- Date - String/baitap.js
+++ Date - String/baitap.js
@@ -16,17 +16,17 @@
 //  			Kết quả: "Monday, Thursday, Sunday"
 //-------------------------------------------------------
 weekday = ''
-function Week_of_day(weekday) {
+function day_of_week(weekday) {
     full_day = 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'
     last_week_day = 'Saturday, Sunday'
     if (weekday === full_day) {
         console.log('everyday')
     }
-    if (weekday === last_week_day) {
+    else if (weekday === last_week_day) {
         console.log('weekend')
     } else {
         result = weekday.replace(/[+,]/g, ',');
         console.log(result)
     }
 }
-Week_of_day('Monday+ Tuesday, Wednesday')
+day_of_week('Saturday, Sunday')
