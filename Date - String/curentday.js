4db74eab77244bb1a2a428ff40b86d9d19604e9b
DESKTOP-J583M93\bienn <bienngocduy@gmail.com>
2018-01-10 23:21:40 +0700

    add review code by P

--- Date - String/curentday.js
+++ Date - String/curentday.js
@@ -1,12 +1,18 @@
 //viet ham lay ngay hien tai
 
+// let currentday = function () {
+//     today = new Date();
+//     let dd = today.getDate();
+//     let mm = today.getMonth() + 1; //As January is 0.
+//     let yyyy = today.getFullYear();
+//     if (dd < 10) dd = '0' + dd;
+//     if (mm < 10) mm = '0' + mm;
+//     return (mm + '/' + dd + '/' + yyyy);
+// };
+// console.log(currentday('/'));
+
 let currentday = function () {
-    today = new Date();
-    let dd = today.getDate();
-    let mm = today.getMonth() + 1; //As January is 0.
-    let yyyy = today.getFullYear();
-    if (dd < 10) dd = '0' + dd;
-    if (mm < 10) mm = '0' + mm;
-    return (mm + '/' + dd + '/' + yyyy);
+    d = new Date();
+    return d.toLocaleString();       // -> "2/1/2013 7:37:08 AM"
 };
-console.log(currentday('/'));
+console.log(currentday());
--- Date - String/test.js
+++ Date - String/test.js
@@ -1,3 +1,15 @@
-n = 'hi+ Saturday + hi'
-a = n.replace(/[+]/g,',')
+x = 'saturday'
+n = 'Saturday+ Sunday'
+a = n.replace(/[ +]/g,'').toLowerCase().trim();
+c = a.match('hi')
+console.log(x)
+
 console.log(a)
+
+console.log(c)
+
+
+
+if(x == c){
+    
+}
